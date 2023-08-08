b79aa80fb5ef21f5fa90f505a153fd159f1ba0be
mcarri355 <carrillomatisa@gmail.com>
2023-08-08 14:35:28 -0700

    extra features

--- scripts/imageSwap.js
+++ scripts/imageSwap.js
@@ -7,42 +7,42 @@ let counter2 = 0;
 let counter3 = 0;
 let counter4 = 0;
 
-function indexSwap(){
-    if(counter1%2 == 0){
-    index.style.backgroundImage = 'url(././photos/index2.jpg)'
-    }
-    if(counter1%2 == 1){
-        index.style.backgroundImage = 'url(././photos/index1.png)'
-    }
-    counter1++;
+function indexSwap() {
+  if (counter1 % 2 == 0) {
+    index.style.backgroundImage = 'url(././photos/index2.jpg)';
+  }
+  if (counter1 % 2 == 1) {
+    index.style.backgroundImage = 'url(././photos/index1.png)';
+  }
+  counter1++;
 }
 
-function campSwap(){
-    if(counter2%2 == 0){
-    index.style.backgroundImage = 'url(././photos/camping1.jpg)'
-    }
-    if(counter2%2 == 1){
-        index.style.backgroundImage = 'url(././photos/camping2.jpg)'
-    }
-    counter2++;
+function campSwap() {
+  if (counter2 % 2 == 0) {
+    index.style.backgroundImage = 'url(././photos/camping1.jpg)';
+  }
+  if (counter2 % 2 == 1) {
+    index.style.backgroundImage = 'url(././photos/camping2.jpg)';
+  }
+  counter2++;
 }
 
-function gameSwap(){
-    if(counter3%2 == 0){
-    index.style.backgroundImage = 'url(././photos/game1.jpg)'
-    }
-    if(counter%2 == 1){
-        index.style.backgroundImage = 'url(././photos/game2.png)'
-    }
-    counter3++;
+function gameSwap() {
+  if (counter3 % 2 == 0) {
+    index.style.backgroundImage = 'url(././photos/game1.jpg)';
+  }
+  if (counter3 % 2 == 1) {
+    index.style.backgroundImage = 'url(././photos/game2.png)';
+  }
+  counter3++;
 }
 
-function jobSwap(){
-    if(counter4%2 == 0){
-    index.style.backgroundImage = 'url(././photos/job1.png)'
-    }
-    if(counter4%2 == 1){
-        index.style.backgroundImage = 'url(./../../photos/job2.png)'
-    }
-    counter4++;
+function jobSwap() {
+  if (counter4 % 2 == 0) {
+    index.style.backgroundImage = 'url(././photos/job1.png)';
+  }
+  if (counter4 % 2 == 1) {
+    index.style.backgroundImage = 'url(./../../photos/job2.png)';
+  }
+  counter4++;
 }
